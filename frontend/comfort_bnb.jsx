2f8393bf879c358917c9701e3f13821c177c3ccd
Jon Chen <jonathan.chen.6799@gmail.com>
2021-07-02 16:12:37 -0500

    Nearly completed user auth

--- frontend/comfort_bnb.jsx
+++ frontend/comfort_bnb.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {login, logout, signup} from "./util/session_api_util"
+import Root from "./components/root"
+import configureStore from "./store/store"
+import {login} from './actions/session_actions'
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById("root");
+  const store = configureStore()
+  window.getState = store.getState
+  window.dispatch = store.dispatch
   window.login = login
-  window.logout = logout
-  window.signup = signup
 
-  ReactDOM.render(<h1>Test 2</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 })
---
+++ frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SplashContainer from "./splash/splash_container"
+
+const App = () => (
+  <div>
+    <h1>App And Root Test</h1>
+    <SplashContainer />
+  </div>
+);
+
+export default App;
