61f561f2f541c3900b745798b43fb3d750f236c1
Jon Chen <jonathan.chen.6799@gmail.com>
2021-07-05 03:26:26 -0500

    Heroku bg

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module ComfortBnB
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
-    config.serve_static_assets = true
-    config.assets.compile = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
