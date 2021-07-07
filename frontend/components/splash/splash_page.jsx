39284e720d3554e8eb44f073be81e4cc15703814
Jon Chen <jonathan.chen.6799@gmail.com>
2021-07-06 23:50:55 -0500

    Improved splash page and changed background image

--- frontend/components/splash/splash_page.jsx
+++ frontend/components/splash/splash_page.jsx
@@ -13,8 +13,15 @@ class SplashPage extends React.Component {
     return (
       <div className="splash-search">
         <div className="splash-background"></div>
-        <SearchBar />
-        <Link to="/browse" className="splash-body">Explore nearby</Link>
+        <SearchBar  />
+        <div className="splash-content">
+          <Link to="/browse" className="splash-body">
+            <div className="splash-text-background">
+              <div className="splash-text">Take me to a comfort place</div>
+            </div>
+            </Link>
+          <h1 className="splash-explore"> Explore nearby</h1>
+        </div>
       </div>
     )
   }
