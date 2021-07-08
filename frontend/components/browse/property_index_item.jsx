17a628f09472a51350f428f1a4f18b2297b473a2
Jon Chen <jonathan.chen.6799@gmail.com>
2021-07-08 09:29:41 -0500

    Added more property index css

--- frontend/components/browse/property_index_item.jsx
+++ frontend/components/browse/property_index_item.jsx
@@ -16,10 +16,26 @@ class PropertyIndexItem extends React.Component {
           Image Placeholder
         </div>
         <Link to={`/browse/${property.id}`} className="property-content">
-          <div className="property-description" >{property.description}</div>
-          <br />
-          Max guests: {property.max_guests}
-          <br />
+          <div className="property-description" >{property.description} </div>
+
+          <div className="property-capacity" >
+            <div clasName="property-capacity-guest">{property.max_guests === 1 ? "1 guest" : `${property.max_guests} guests`}</div>
+            <div> <div className="bullet-market">&#183; </div> {property.num_bedrooms === 1 ? "1 bedroom" : `${property.num_bedrooms} bedrooms`}</div>
+            <div> <div className="bullet-market">&#183; </div> {property.num_beds === 1 ? "1 bed" : `${property.num_beds} beds`}</div>
+            <div> <div className="bullet-market">&#183; </div> {property.num_baths === 1 ? "1 bath" : `${property.num_baths} baths`}</div>
+          </div>
+          <div className="property-amenities">
+            Amenities Placeholder
+          </div>
+          <div className="property-content-bottom"> 
+            <div className="property-review-rating">
+              Review Rating Placeholder
+            </div>
+            <div className="property-price">
+              Price Placeholder
+            </div>
+          </div>
+
         </Link>
       </div>
     )
