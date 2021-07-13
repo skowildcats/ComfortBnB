2ac036bba3aae885dc8b6a1fcbb5c56dca8ade52
Jon Chen <jonathan.chen.6799@gmail.com>
2021-07-13 18:33:33 -0500

    Custom marker

--- frontend/components/browse/browse_map.jsx
+++ frontend/components/browse/browse_map.jsx
@@ -15,7 +15,15 @@ class BrowseMap extends React.Component {
 
     this.map = new google.maps.Map(this.mapNode, mapOptions);
 
-    
+    var icon = {
+      url: 'https://i.imgur.com/HB5Cvjd.png',
+      // This marker is 50 pixels wide by 50 pixels high.
+      size: new google.maps.Size(40, 30),
+      // The origin for this image is (0, 0).
+      origin: new google.maps.Point(0, -2 ),
+      // The anchor for this image is the base of the flagpole at (0, 32).
+      anchor: new google.maps.Point(0, 30)
+    };
 
     for (let i = 0; i < properties.length; i++) {
       let infowindow = new google.maps.InfoWindow({
@@ -32,6 +40,7 @@ class BrowseMap extends React.Component {
       label: "$" + String(properties[i].price),
       map: this.map,
       animation: google.maps.Animation.DROP,
+        icon: icon
       })
       marker.addListener("click", () => { 
         infowindow.open({
