f91094c82dc39a7fd271e4dacec4390a4e4beb64
Jon Chen <jonathan.chen.6799@gmail.com>
2021-08-04 10:42:43 -0500

    Added portfolio icon

--- frontend/components/splash/splash_auth.jsx
+++ frontend/components/splash/splash_auth.jsx
@@ -127,10 +127,11 @@ class SplashAuth extends React.Component{
       </div>
       <div className="profile-dropdown">
         <div className="icon-list">
+          <a href="https://www.linkedin.com/in/jon-chen67/" target="_blank"><i class="fas fa-folder-open"></i></a>  
           <a href="https://github.com/skowildcats" target="_blank"><i className="fab fa-github"></i></a>
           <a href="https://www.linkedin.com/in/jon-chen67/" target="_blank"><i className="fab fa-linkedin"></i></a>
-          <a href="https://www.linkedin.com/in/jon-chen67/" target="_blank"><i className="fab fa-angellist"></i></a>
-        </div>
+          <a href="https://angel.co/u/jonathan-chen-30" target="_blank"><i className="fab fa-angellist"></i></a>
+        </div>  
         <button onClick={this.toggleDropDown.bind(this)} className="profile-list-icon">
           <i className="profile-list fas fa-bars"></i>
           {userIcon}
